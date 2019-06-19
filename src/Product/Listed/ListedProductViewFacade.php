pesovo/shopsys

all product data on the search page is obtained from elasticsearch

packages/read-model/src/Product/Listed/ListedProductViewFacade.php
@@ -192,7 +192,7 @@ class ListedProductViewFacade implements ListedProductViewFacadeInterface
     {
         $paginationResult = $this->productOnCurrentDomainFacade->getPaginatedProductsForSearch($searchText, $filterData, $orderingModeId, $page, $limit);
 
-        return $this->createPaginationResultWithData($paginationResult);
+        return $this->createPaginationResultWithHits($paginationResult);
     }
 
     /**

shopsys/read-model

src/Product/Listed/ListedProductViewFacade.php
@@ -192,7 +192,7 @@ class ListedProductViewFacade implements ListedProductViewFacadeInterface
     {
         $paginationResult = $this->productOnCurrentDomainFacade->getPaginatedProductsForSearch($searchText, $filterData, $orderingModeId, $page, $limit);
 
-        return $this->createPaginationResultWithData($paginationResult);
+        return $this->createPaginationResultWithArray($paginationResult);
     }
 
     /**