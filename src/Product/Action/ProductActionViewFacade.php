pesovo/shopsys

all product data on category detail is obtained from elasticsearch

packages/read-model/src/Product/Action/ProductActionViewFacade.php
@@ -59,4 +59,13 @@ class ProductActionViewFacade
 
         return $productActionViews;
     }
+
+    /**
+     * @param array $hit
+     * @return \Shopsys\ReadModelBundle\Product\Action\ProductActionView
+     */
+    public function getForHit(array $hit): ProductActionView
+    {
+        return $this->productActionViewFactory->createFromHit($hit);
+    }
 }

shopsys/read-model

src/Product/Action/ProductActionViewFacade.php
@@ -59,4 +59,13 @@ class ProductActionViewFacade
 
         return $productActionViews;
     }
+
+    /**
+     * @param array $productArray
+     * @return \Shopsys\ReadModelBundle\Product\Action\ProductActionView
+     */
+    public function getForArray(array $productArray): ProductActionView
+    {
+        return $this->productActionViewFactory->createFromArray($productArray);
+    }
 }